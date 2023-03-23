484a16119e20ce95ea269e1807a62b18f989d964
Illia Myronov <Illya.myronov@gmail.com>
2023-03-23 09:57:41 -0500

    Main Page 2 buttons

--- Analog/ViewViewModels/Main/MainViewModel.cs
+++ Analog/ViewViewModels/Main/MainViewModel.cs
@@ -3,14 +3,36 @@ using MyFirstProject.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Analog.ViewViewModels.Main
 {
     class MainViewModel : BaseViewModel
     {
+        public ICommand OnCameraClicked { get; set; }
+        public ICommand OnGalleryClicked { get; set; }
+
         public MainViewModel()
         {
             Title = Titles.MainTitle;
+
+            OnCameraClicked = new Command(OnCameraClickedAsync);
+            OnGalleryClicked = new Command(OnGalleryClickedAsync);
+        }
+
+        private async void OnCameraClickedAsync(Object obj)
+        {
+
+            await Application.Current.MainPage.Navigation.PushAsync(new LayoutView());
+
+        }
+
+        private async void OnGalleryClickedAsync(Object obj)
+        {
+
+            await Application.Current.MainPage.Navigation.PushAsync(new LayoutView());
+
         }
     }
 }
