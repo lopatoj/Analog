13130fb514bd2b68ee88c7a980adc093dfd9c26a
Hanyu W <HyWei787@gmail.com>
2023-03-23 10:11:03 -0500

    android v10

--- Analog/ViewViewModels/Main/MainViewModel.cs
+++ Analog/ViewViewModels/Main/MainViewModel.cs
@@ -24,14 +24,14 @@ namespace Analog.ViewViewModels.Main
         private async void OnCameraClickedAsync(Object obj)
         {
 
-            await Application.Current.MainPage.Navigation.PushAsync(new LayoutView());
+            //await Application.Current.MainPage.Navigation.PushAsync(new LayoutView());
 
         }
 
         private async void OnGalleryClickedAsync(Object obj)
         {
 
-            await Application.Current.MainPage.Navigation.PushAsync(new LayoutView());
+            //await Application.Current.MainPage.Navigation.PushAsync(new LayoutView());
 
         }
     }
