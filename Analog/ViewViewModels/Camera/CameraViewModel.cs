af0cac5269a97ed863ffff2e3b5227b20ff05fb1
Justin Lop <Justin@lopato.org>
2023-03-23 10:26:00 -0500

    yippie

---
+++ Analog/ViewViewModels/Camera/CameraViewModel.cs
@@ -0,0 +1,12 @@
+﻿using MyFirstProject.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Analog.ViewViewModels.Camera
+{
+    class CameraViewModel : BaseViewModel
+    {
+
+    }
+}
