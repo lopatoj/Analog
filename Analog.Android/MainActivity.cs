0870f76fe2686ce92aae1bc364b60eae7eda13f2
Illia Myronov <Illya.myronov@gmail.com>
2023-03-29 09:46:30 -0500

    vd

--- Analog.Android/MainActivity.cs
+++ Analog.Android/MainActivity.cs
@@ -4,6 +4,8 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using AndroidX.Core.Content;
+using Android;
 
 namespace Analog.Droid
 {
@@ -16,6 +18,14 @@ namespace Analog.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.Camera) != (int)Permission.Granted || ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation) != (int)Permission.Granted)
+            {
+                RequestPermissions(new string[] { Manifest.Permission.Camera, Manifest.Permission.AccessFineLocation }, 0);
+            }
+
+
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
