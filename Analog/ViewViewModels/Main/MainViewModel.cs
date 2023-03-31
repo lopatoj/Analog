a6343dccc8c3c49395450dda50a142e6b5cdb509
Justin Lop <Justin@lopato.org>
2023-03-31 10:11:29 -0500

    ai stuff

--- Analog/ViewViewModels/Main/MainViewModel.cs
+++ Analog/ViewViewModels/Main/MainViewModel.cs
@@ -17,12 +17,32 @@ namespace Analog.ViewViewModels.Main
         public ICommand OnGalleryClicked { get; set; }
         public Byte[] imgAsBytes;
 
+        ClockScan _clockScan;
+
+        private Byte[] imgAsBytes;
+
         public MainViewModel()
         {
             Title = Titles.MainTitle;
 
             OnCameraClicked = new Command(OnCameraClickedAsync);
             OnGalleryClicked = new Command(OnGalleryClickedAsync);
+
+            _clockScan = new ClockScan();
+        }
+
+        async Task RunInferenceAsync()
+        {
+            try
+            {
+                var image = imgAsBytes;
+                var result = await _clockScan.GetClassificationAsync(image);
+                await Application.Current.MainPage.DisplayAlert("The time on your clock is:", result, "OK");
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+            }
         }
 
         private async void OnCameraClickedAsync(Object obj)
