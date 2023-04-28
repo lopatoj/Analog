88c3f7fc82e91e97797262b2c549bb4f87b38579
H W <Noob101NOOBS@outlook.com>
2023-04-28 10:39:41 -0500

    input 3/224 are flipped????

--- Analog/Models/ClockScan.cs
+++ Analog/Models/ClockScan.cs
@@ -10,6 +10,8 @@ using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Advanced;
 using System.Collections.Generic;
 using System.Linq;
+using Python.Runtime;
+using Numpy;
 
 namespace Analog.Models
 {
@@ -24,6 +26,8 @@ namespace Analog.Models
             modelStream.CopyTo(modelMemoryStream);
             byte[] _model = modelMemoryStream.ToArray();
             InferenceSession _session = new InferenceSession(_model);
+            
+
 
             using var modelStream_stn = assembly.GetManifestResourceStream("Analog.Resources.analog_stn.onnx");
             using var modelMemoryStream_stn = new MemoryStream();
@@ -45,10 +49,8 @@ namespace Analog.Models
             });
             img.Save(imageStream, format);
 
-            // Preprocess image
-            Tensor<float> input = new DenseTensor<float>(new[] { 1, 3, 224, 224 });
-            var mean = new[] { 0.485f, 0.456f, 0.406f };
-            var stddev = new[] { 0.229f, 0.224f, 0.225f };
+            // Preprocess image 
+            Tensor<float> input = new DenseTensor<float>(new[] { 1, 224, 3, 224 });
             img.ProcessPixelRows(accessor =>
             {
                 for (int y = 0; y < accessor.Height; y++)
@@ -56,39 +58,12 @@ namespace Analog.Models
                     Span<Rgb24> pixelSpan = accessor.GetRowSpan(y);
                     for (int x = 0; x < accessor.Width; x++)
                     {
-                        input[0, 0, y, x] = ((pixelSpan[x].R / 255f) - mean[0]) / stddev[0];
-                        input[0, 1, y, x] = ((pixelSpan[x].G / 255f) - mean[1]) / stddev[1];
-                        input[0, 2, y, x] = ((pixelSpan[x].B / 255f) - mean[2]) / stddev[2];
+                        input[0, x, 0, y] = (pixelSpan[x].R / 255f);
+                        input[0, x, 1, y] = (pixelSpan[x].G / 255f);
+                        input[0, x, 2, y] = (pixelSpan[x].B / 255f);
                     }
                 }
             });
-
-            // Run inference
-            //using IDisposableReadOnlyCollection<DisposableNamedOnnxValue> results = _session.Run(input);
-
-            // Postprocess to get softmax vector
-            //IEnumerable<float> output = results.First().AsEnumerable<float>();
-            //float sum = output.Sum(x => (float)Math.Exp(x));
-            //IEnumerable<float> softmax = output.Select(x => (float)Math.Exp(x) / sum);
-
-            // Extract top 10 predicted classes
-            //IEnumerable<Prediction> top10 = softmax.Select((x, i) => new Prediction { Label = LabelMap.Labels[i], Confidence = x })
-                               //.OrderByDescending(x => x.Confidence)
-                               //.Take(10);
-
-            // Print results to console
-           // Console.WriteLine("Top 10 predictions for ResNet50 v2...");
-            //Console.WriteLine("--------------------------------------------------------------");
-            //foreach (var t in top10)
-            //{
-                //Console.WriteLine($"Label: {t.Label}, Confidence: {t.Confidence}");
-            //}
-            // Create Tensor model input
-            // The model expects input to be in the shape of (N x 3 x H x W) i.e.
-            // mini-batches (where N is the batch size) of 3-channel RGB images with H and W of 224
-            // https://onnxruntime.ai/docs/api/csharp-api#systemnumericstensor
-
-            //var input = new DenseTensor<float>(channelData, new[] { DimBatchSize, DimNumberOfChannels, ImageSizeX, ImageSizeY });
 
             // Run inferencing
             // https://onnxruntime.ai/docs/api/csharp-api#methods-1
@@ -100,20 +75,27 @@ namespace Analog.Models
             // https://github.com/onnx/models/tree/master/vision/classification/mobilenet#output
             // https://onnxruntime.ai/docs/api/csharp-api#disposablenamedonnxvalue
 
-            var output = results.FirstOrDefault(i => i.Name == "495");
-
-
+            //var output = results.LastOrDefault(i => i.Name == "495");
 
             // Postprocess output (get highest score and corresponding label)
             // https://github.com/onnx/models/tree/master/vision/classification/mobilenet#postprocessing
 
-            var scores = output.AsTensor<float>().ToList();
-            var highestScore = scores.Max();
-            var time = Math.Floor(highestScore / 60) + ":" + highestScore % 60; // returns time as string in format: hours:minutes
+            IEnumerable<float> output = results.First(i => i.Name == "495").AsEnumerable<float>();
+            //float sum = output.Sum(x => (float)Math.Exp(x));
+            //Console.WriteLine(sum.ToString());
+            //IEnumerable<float> softmax = output.Select(x => (float)Math.Exp(x) / sum);
+            
+            var scores = output.ToList();
+            for(int i = 0; i < scores.Count; i++)
+            {
+                scores[i] = Math.Abs(scores[i]);
+                Console.WriteLine(scores[i].ToString());
+            }
+            _session.Dispose();
+            //float newtime = sum * 60;
+            //string time = Math.Floor(newtime / 60) + ":" + Math.Round(newtime % 60);
 
-            //session.Dispose();
-
-            return time;
+            return scores.Average()+ "";
         }
     }
 }
---
+++ Analog/Models/LinearLayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace Analog.Models
+{
+    public class LinearLayer
+    {
+        private Matrix<byte> weights;
+        private Vector<byte> biases;
+
+        public LinearLayer(int inputSize, int outputSize)
+        {
+            weights = Matrix<byte>.Build.Random(inputSize, outputSize);
+            biases = Vector<byte>.Build.Dense(outputSize);
+        }
+
+        public Vector<byte> Forward(Vector<byte> input)
+        {
+            var output = input * weights + biases;
+            return output;
+        }
+    }
+}
